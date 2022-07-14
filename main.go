492f9a74734c9e223a6028eae8bea2a132d0004d
MOMO <momo@MOMOdeMacBook-Pro.local>
2022-07-14 14:04:09 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+	_ "hello-api/routers"
+)
+
+func main() {
+
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
